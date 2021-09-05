bc4d4e36f02cd40fec2b950b4fc04db974ac931a
rahuldhodapkar <rahul.dhodapkar@yale.edu>
2021-09-05 19:27:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# NK/T cell lymphoma of the orbit and adnexa
+Literature review of NK/T cell lymphoma involving the orbit and adnexa
+
+## Inclusion Criteria
+
+- Pubmed indexed
+- English language
+- Case description of NK/T cell lymphoma involving the orbit and adnexa
+
+## Exclusion Criteria
+
+- Did not have full description of patient age, sex, and particular orbital invovement
+- Did not have description of treatment
+- Did not have description of outcome
+
+## Methods
+
+Using the PRISMA guidelines, searches were performed on pubmed on 9/3/21 using a predetermined
+set of search criteria. Articles were evaluated manually on the basis of title and abstract as to
+whether they fit the inclusion and exclusion criteria. Only primary articles describing cases were
+included, if articles were literature reviews or summaries of previously described cases, citations
+for the original article with case descriptions were extracted from the review references and
+subsequently evaluated using the inclusion and exclusion criteria above.
+
---
+++ src/MergeSearches.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+#
+## MergeSearches.R
+#
+# Merge data from searches to identify the number of duplicates
+#
+
+library(stringr)
+library(hashmap)
+
+search.metadata <- read.csv2('./data/search_metadata.csv', quote='', sep=',')
+
+search.data <- NULL
+for (fn in search.metadata$filename) {
+    x <- read.csv(paste0('./data/searches/', fn))
+
+    if (is.null(search.data)) {
+        search.data <- x
+    } else {
+        search.data <- rbind(search.data,x)
+    }
+}
+
+print(paste0("Initial Search Results: ", dim(search.data)[[1]]))
+
+dedup.data <- data.frame(
+    PMID = unique(search.data$PMID)
+)
+
+dedup.data$Title <-            hashmap(search.data$PMID, search.data$Title)[[dedup.data$PMID]]
+dedup.data$Publication.Year <- hashmap(search.data$PMID, search.data$Publication.Year)[[dedup.data$PMID]]
+dedup.data$Authors <-          hashmap(search.data$PMID, search.data$Authors)[[dedup.data$PMID]]
+
+write.csv(dedup.data, './calc/deduplicated_searches.csv', row.names=F)
+
+print(paste0("Deduplicated Search Results: ", dim(dedup.data)[[1]]))
+
+print("All done!")
